4c5f7ccad939bcbcf36135920974b203870aa3fb
Igor Czeszejko-Sochacki <igor.cz@op.pl>
2020-06-11 21:53:35 +0200

    Changes to GetProject endpoint

--- TimeApp.Service/ProjectService.cs
+++ TimeApp.Service/ProjectService.cs
@@ -13,26 +13,31 @@ namespace TimeApp.Service
     public class ProjectService : IProjectService
     {
         private readonly IRepository<Project> _projectrepo;
-        public ProjectService(IRepository<Project> projectrepo)
+        private readonly IRepository<MainProject> _mainprojectrepo;
+        public ProjectService(IRepository<Project> projectrepo, IRepository<MainProject> mainprojectrepo)
         {
             _projectrepo = projectrepo;
+            _mainprojectrepo = mainprojectrepo;
         }
 
         public async Task<List<ProjectDTO>> GetAllProjectsTotal()
         {
             var projectList = await _projectrepo.GetAll();
             var finalList = new List<ProjectDTO>();
+            var mainProjectList = await _mainprojectrepo.GetAll();
+            foreach (MainProject mainProject in mainProjectList)
+            {
+                finalList.Add(new ProjectDTO()
+                {
+                    Name = mainProject.Name,
+                    WorkedHours = 0
+                }) ;
+            }
+
             foreach (Project project in projectList)
             {
-                if (!finalList.Any(name => name.Name == project.Name))
-                {
-                    finalList.Add(new ProjectDTO()
-                    {
-                        Name = project.Name,
-                        WorkedHours = project.WorkedHours
-                    });
-                }
-                else
+
+                if (finalList.Any(name => name.Name == project.Name))
                 {
                     var temp = finalList.Find(name => name.Name == project.Name);
                     temp.WorkedHours = temp.WorkedHours + project.WorkedHours;
--- TimeApp.Service/RaportService.cs
+++ TimeApp.Service/RaportService.cs
@@ -125,6 +125,7 @@ namespace TimeApp.Service
                 {
                    Name = name
                 });
+
             }
             catch (Exception e)
             {
